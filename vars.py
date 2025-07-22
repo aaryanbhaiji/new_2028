e13c11f423ac945109ab3f641357d307b742e003
aaryan <rajaaryanskr8032@gmail.com>
2025-07-22 16:00:36 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,7 +13,7 @@ CREDIT = environ.get("CREDIT", "@heroku_cc_bot")
 TOTAL_USER = os.environ.get('TOTAL_USERS', '953685850').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '953685850,6009370025').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '953685850').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
